72da281e5c2b88ac45c334a252b906c5f65d62c5
Stan He <49423142+StanBHe@users.noreply.github.com>
2023-11-01 20:59:00 -0700

    Have Interrupt Handler Read and Clear Video and Command Interrupts

--- Firmware/src/startup.c
+++ Firmware/src/startup.c
@@ -31,6 +31,7 @@ __attribute__((always_inline)) inline void csr_disable_interrupts(void){
     asm volatile ("csrci mstatus, 0x8");
 }
 
+#define INTERRUPT_PENDING_REGISTER (*((volatile uint32_t *)0x40000004))
 #define MTIME_LOW       (*((volatile uint32_t *)0x40000008))
 #define MTIME_HIGH      (*((volatile uint32_t *)0x4000000C))
 #define MTIMECMP_LOW    (*((volatile uint32_t *)0x40000010))
@@ -61,11 +62,22 @@ extern volatile int global;
 extern volatile uint32_t controller_status;
 
 void c_interrupt_handler(void){
-    uint64_t NewCompare = (((uint64_t)MTIMECMP_HIGH)<<32) | MTIMECMP_LOW;
-    NewCompare += 100;
-    MTIMECMP_HIGH = NewCompare>>32;
-    MTIMECMP_LOW = NewCompare;
-    global++;
-    controller_status = CONTROLLER;
+
+    if(INTERRUPT_PENDING_REGISTER & 0x2){
+        int arand = 1;
+        uint64_t NewCompare = (((uint64_t)MTIMECMP_HIGH)<<32) | MTIMECMP_LOW;
+        NewCompare += 100;
+        MTIMECMP_HIGH = NewCompare>>32;
+        MTIMECMP_LOW = NewCompare;
+        global++;
+        controller_status = CONTROLLER;
+        INTERRUPT_PENDING_REGISTER = (INTERRUPT_PENDING_REGISTER & 0xFFFB);
+    }
+
+    if(INTERRUPT_PENDING_REGISTER & 0x4){
+        int arand = 2;
+
+        INTERRUPT_PENDING_REGISTER = (INTERRUPT_PENDING_REGISTER & 0xFFFD);
+    }
 }
 
