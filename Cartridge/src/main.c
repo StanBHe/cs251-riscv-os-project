bb7d96f668d79d12b99580d35ca9213729be703b
Gia-Phong Nguyen <giatheawesome2@gmail.com>
2023-12-06 01:26:18 -0800

    t

--- Cartridge/src/main.c
+++ Cartridge/src/main.c
@@ -104,39 +104,6 @@ void printCount(int count);
     Block types:
 */
 
-void otherThreadFunc() {
-    int last_global = 42;
-    uint32_t global = 42;
-    uint32_t controller_status = 0;
-    int last_reset = GetReset();
-    int reset;
-    int total = 0;
-
-    drawText(2, 6, "Thread 2");
-    while (1) {
-        reset = GetReset();
-        global = GetTicks();
-        if(global != last_global) {
-            drawText(2, 4, "Thread 2");
-            controller_status = GetController();
-            if(controller_status){
-                if(controller_status & 0x2){
-                }
-                if(controller_status & 0x8){
-                    drawText(2, 12, "attempt switch from 2");
-                    SwitchThread(other_thread, &MainThread);
-                }
-            }
-
-            last_global = global;
-            if(last_reset != reset){
-                last_reset = reset;
-            }
-        }
-    }
-    //return a + b;
-}
-
 int main() {
     int last_global = 42;
     uint32_t global = 42;
